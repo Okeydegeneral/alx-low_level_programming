86f216b0fbc059baf2e43387e7219297aafa5845
Okeydegeneral <okechukwu1414@gmail.com>
2022-03-11 09:39:51 -0800

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
+
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,\in");
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0)
 }
